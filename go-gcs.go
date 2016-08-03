531114d84513c054914ee546b07c7caa8985bb06
daiiz <da.iz.mg@gmail.com>
2016-08-03 23:54:04 +0900

    コマンド実行

--- go-gcs.go
+++ go-gcs.go
@@ -2,9 +2,27 @@ package main
 
 import (
     "fmt"
+    "flag"
 )
 
+var (
+    bucketName = flag.String("b", "", "GCS Bucket name")
+    filePath   = flag.String("gf", "", "GCS filePath")
+    doTask     = flag.String("do", "dl", "<dl | up>")
+)
+
+// gcsgcs -do dl -b daiiz-bucket-1 -gf public/blank.png
 func main() {
-    savedPath, _ := Download("daiiz-bucket-1", "public/blank.png")
-    fmt.Println(savedPath)
+    flag.Parse()
+
+    if *doTask == "dl" {
+        if (len(*bucketName) > 0 && len(*filePath) > 0) {
+            savedPath, err := Download(*bucketName, *filePath)
+            if err != nil {
+                fmt.Println(err)
+                return
+            }
+            fmt.Println(savedPath)
+        }
+    }
 }
