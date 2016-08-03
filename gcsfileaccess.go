137a1fd706a0f7eade71e065b88d3f43f3e6d340
daiiz <da.iz.mg@gmail.com>
2016-08-03 23:00:13 +0900

    ファイルをfetchしてsave

--- gcsfileaccess.go
+++ gcsfileaccess.go
@@ -2,23 +2,63 @@ package gcs
 
 import (
     "os"
-    "fmt"
+    "strings"
+    "path/filepath"
     "bytes"
     "google.golang.org/cloud/storage"
     "golang.org/x/net/context"
+    "github.com/mitchellh/go-homedir"
 )
 
-func SaveToLocal(filePath string, content []byte) {
+// 任意の場所に保存する
+func SaveLocal(filePath string, content []byte) (string, error) {
     file, err := os.Create(filePath)
     if err != nil {
-        fmt.Print(err)
-        return
+        return "", err
     }
     file.Write(content)
     defer file.Close()
+    return filePath, nil
 }
 
-func Get(bucket, filePath string) ([]byte, error) {
+// ~/gcs_buckets/<bucket>/ にファイルを保存する
+// ディレクトリ構造を維持する
+func Download(bucket, filePath string) (string, error) {
+    home, _ := homedir.Dir()
+
+    dirs := strings.Split(filePath, "/")
+    fileDir := ""
+    fileName := dirs[len(dirs) - 1]
+    for i := 0; i < len(dirs) - 1; i++ {
+        fileDir += dirs[i] + "/"
+    }
+
+    dir := filepath.Join(home, "gcs_buckets")
+    dir = filepath.Join(dir, bucket)
+    dir = filepath.Join(dir, fileDir)
+
+    // ローカルのホームにGCSのbucket階層構造を生成する
+    if err := os.MkdirAll(dir, 0777); err != nil {
+        return "", err
+    }
+
+    // GCSからファイル内容を取得する
+    content, err := Fetch(bucket, filePath);
+    if err != nil {
+        return "", err
+    }
+
+    // ファイルをローカルに保存する
+    savedFilePath, err := SaveLocal(filepath.Join(dir, fileName), content)
+    if err != nil {
+        return "", err
+    }
+
+    return savedFilePath, nil
+}
+
+
+func Fetch(bucket, filePath string) ([]byte, error) {
     ctx := context.Background()
     client, err := storage.NewClient(ctx)
     if err != nil {
